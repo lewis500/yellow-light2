9bb8405dc92c783377afcfa8158ef675a04c452b
lewis500 <lewis500@gmail.com>
2019-06-26 14:40:53 -0500

    switched to using reducer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,10 @@
-import React, { createElement as CE, FunctionComponent, useState } from "react";
+import React, {
+  createElement as CE,
+  FunctionComponent,
+  useContext,
+  useState,
+  useReducer
+} from "react";
 // import style from "./styleApp.scss";
 import Button from "@material-ui/core/Button";
 import Slider from "@material-ui/lab/Slider";
@@ -12,6 +18,7 @@ import Vis from "src/components/Vis";
 import memoizeone from "memoize-one";
 import { params, widths } from "src/constants";
 import { makeStyles } from "@material-ui/styles";
+import { number } from "prop-types";
 
 const useStyles = makeStyles({
   "@global": {
@@ -118,14 +125,111 @@ const getxssd = memoizeone(
 );
 const getxcl = memoizeone((v0: number, yellow: number) => v0 * yellow);
 
+const initialState = {
+  play: false,
+  v0: params.v0,
+  x0: 15,
+  stopper: {
+    x: widths.start,
+    v: params.v0
+  },
+  mover: {
+    x: widths.start,
+    v: params.v0
+  },
+  time: 0,
+  useState: 2,
+  yellow: 2
+};
+type CarType = { x: number; v: number };
+type ActionTypes =
+  | {
+      type: "TICK";
+      payload: { time: number; mover: CarType; stopper: CarType };
+    }
+  | {
+      type: "SET_TIME";
+      payload: number;
+    }
+  | { type: "SET_X0"; payload: number }
+  | { type: "SET_V0"; payload: number }
+  | { type: "SET_YELLOW"; payload: number }
+  | { type: "RESTART" }
+  | { type: "RESET" }
+  | { type: "SET_PLAY"; payload: boolean };
+
+const reducer = (
+  state: typeof initialState = initialState,
+  action: ActionTypes
+): typeof initialState => {
+  let { type, payload } = action;
+  switch (type) {
+    case "TICK":
+      let { time, mover, stopper } = payload;
+      return {
+        ...state,
+        time,
+        mover,
+        stopper
+      };
+    case "SET_TIME":
+      return {
+        ...state,
+        time: payload
+      };
+    case "SET_X0":
+      return {
+        ...state,
+        x0: payload
+      };
+    case "SET_PLAY":
+      return {
+        ...state,
+        play: payload
+      };
+    case "SET_YELLOW":
+      return {
+        ...state,
+        yellow: payload
+      };
+    case "SET_V0":
+      return {
+        ...state,
+        v0: payload
+      };
+    case "RESTART":
+      return {
+        ...state,
+        mover: {
+          v: state.v0,
+          x: widths.start
+        },
+        stopper: {
+          v: state.v0,
+          x: widths.start
+        }
+      };
+    case "RESET":
+      return {
+        ...state,
+        play: false,
+        mover: {
+          v: state.v0,
+          x: widths.start
+        },
+        stopper: {
+          v: state.v0,
+          x: widths.start
+        }
+      };
+    default:
+      return state;
+  }
+};
+
 const App: FunctionComponent<{}> = () => {
-  const [play, setPlay] = useState(false),
-    [x0, setX0] = useState(widths.start - 10),
-    [v0, setV0] = useState(params.v0),
-    [stopper, setStopper] = useState({ x: widths.start, v: v0 }),
-    [mover, setMover] = useState({ x: widths.start, v: v0 }),
-    [time, setTime] = useState(0),
-    [yellow, setYellow] = useState(2);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { x0, v0, stopper, mover, time, play, yellow } = state;
 
   const classes = useStyles();
   const xssd = getxssd(v0);
@@ -134,21 +238,30 @@ const App: FunctionComponent<{}> = () => {
   useTimer((dt: number) => {
     dt /= params.delta;
     if (mover.x > widths.start - widths.total || stopper.v > 0) {
-      setMover(({ x, v }) => ({ v, x: x - v * dt }));
-      setTime(t => dt + t);
-      setStopper(({ x, v }) => {
-        if (x <= Math.min(xssd - v0 * params.tp, x0))
-          return {
-            v: Math.max(v - params.a * dt, 0),
-            x: Math.min(x - v * dt + 0.5 * params.a * dt * dt, x)
-          };
-        return { v, x: x - v * dt };
+      let newStopper =
+        stopper.x <= Math.min(xssd - v0 * params.tp, x0)
+          ? {
+              v: Math.max(stopper.v - params.a * dt, 0),
+              x: Math.min(
+                stopper.x - stopper.v * dt + 0.5 * params.a * dt * dt,
+                stopper.x
+              )
+            }
+          : { v: stopper.v, x: stopper.x - stopper.v * dt };
+      dispatch({
+        type: "TICK",
+        payload: {
+          mover: {
+            v: mover.v,
+            x: mover.x - mover.v * dt
+          },
+          stopper: newStopper,
+          time: time + dt
+        }
       });
     } else {
       setTimeout(() => {
-        setTime(0);
-        setMover({ x: widths.start, v: v0 });
-        setStopper({ x: widths.start, v: v0 });
+        dispatch({ type: "RESTART" });
       }, 200);
     }
   }, play);
@@ -167,9 +280,10 @@ const App: FunctionComponent<{}> = () => {
       </div>
       <Paper className={classes.paper} elevation={2}>
         {Sliders({
-          setX0,
-          setV0,
-          setYellow,
+          setX0: (x0: number) => dispatch({ type: "SET_X0", payload: x0 }),
+          setV0: (v0: number) => dispatch({ type: "SET_V0", payload: v0 }),
+          setYellow: (yellow: number) =>
+            dispatch({ type: "SET_YELLOW", payload: yellow }),
           x0,
           v0,
           yellow
@@ -178,7 +292,7 @@ const App: FunctionComponent<{}> = () => {
           className={classes.button}
           variant="contained"
           color="secondary"
-          onClick={() => setPlay(play => !play)}
+          onClick={() => dispatch({ type: "SET_PLAY", payload: !play })}
         >
           {play ? "PAUSE" : "PLAY"}
         </Button>
@@ -188,9 +302,7 @@ const App: FunctionComponent<{}> = () => {
           variant="contained"
           color="secondary"
           onClick={() => {
-            setMover(({ x, v }) => ({ v: v0, x: widths.start }));
-            setStopper(({ x, v }) => ({ v: v0, x: widths.start }));
-            setPlay(false);
+            dispatch({ type: "RESET" });
           }}
         >
           Reset
